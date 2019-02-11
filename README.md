caf15c1c501cf188607f720dc6e3cda1f9af0c95
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2019-02-11 17:32:38 +0200

    Fixes and README

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# VSCode Godot C++ Snippets
+# Visual Studio Code Godot Snippets
 
-> Snippets for Godot C++ development.
+### Snippets for Godot development.
 
-![Preview](images/preview.gif)
+Supported languages:
+* C++:
 
-# Installation
+    ![Preview](images/godot-setget-bind-method.gif)
 
-TODO: make this an actual extension?
+## Installation
+
+Currently there's no easy way to install snippets. For manual installation refer to:
+* [Creating your own snippets](https://code.visualstudio.com/docs/editor/userdefinedsnippets#_creating-your-own-snippets)
+
+TODO: make this an actual extension or make contribution to [godot-tools](https://github.com/GodotExplorer/godot-tools/).
+
+## Usage
+1. Type `godot` prefix.
+2. Press <kbd>Ctrl+Space</kbd> to popup intellisense for godot specific snippets.
+3. Select snippet. Use <kbd>Tab</kbd> to jump between snippet's placeholders.
+
+## Contributing
+
+New snippets are added and modified per language in `snippets/src`, then run:
+```bash
+python make_snippets.py
+```
+The resulting snippets are going to be in `snippets` folder.
--- make_snippets.py
+++ make_snippets.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 '''
-Make snippets for Godot C++ development
+Make VSCode snippets for Godot C++ development
 '''
 
 import re
 import os
 
-SNIPPETS_SOURCE_PATH = "src/snippets/"
+SNIPPETS_SOURCE_PATH = "snippets/src/"
 SNIPPETS_DEST_PATH = "snippets/"
 
 PLACEHOLDER_VARIANT_TYPES = "PLACEHOLDER_VARIANT_TYPES"
-PLACEHOLDER_CLASS = "PLACEHOLDER_CLASS"
-
 VARIANT_TYPES = r'''NIL,BOOL,INT,REAL,STRING,VECTOR2,RECT2,VECTOR3,TRANSFORM2D,PLANE,QUAT,AABB,BASIS,TRANSFORM,COLOR,NODE_PATH,_RID,OBJECT,DICTIONARY,ARRAY,POOL_BYTE_ARRAY,POOL_INT_ARRAY,POOL_REAL_ARRAY,POOL_STRING_ARRAY,POOL_VECTOR2_ARRAY,POOL_VECTOR3_ARRAY,POOL_COLOR_ARRAY'''
+
+PLACEHOLDER_CLASS = "PLACEHOLDER_CLASS"
 CLASS = r'''TM_FILENAME_BASE/([A-Za-z0-9]*)_+([A-Za-z0-9]*)/${1:/capitalize}${2:/capitalize}/g'''
 
-def make_snippet():
+def make_snippets():
 	# Read snippet source
-	with open(SNIPPETS_SOURCE_PATH + 'cpp_raw.json', 'r') as snippet:
+	with open(SNIPPETS_SOURCE_PATH + 'cpp.json', 'r') as snippet:
 		f = snippet.read()
 
 	# Replace template stamps
@@ -30,4 +30,4 @@ def make_snippet():
 		snippet.write(f)
 
 if __name__ == "__main__":
-	make_snippet()
+	make_snippets()
